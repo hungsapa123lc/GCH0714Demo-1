15f483773662e7783f5971a6970a1955a259822d
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-05-27 08:51:30 +0700

    check url in request

--- webApp/app.js
+++ webApp/app.js
@@ -1,7 +1,19 @@
 var http = require('http');
 var server = http.createServer((req,res)=>{
-   res.end('Hello world!');
-
+    if(req.url =='/'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p style="color:red">This is Home Page.</p></body></html>');
+        res.end();
+    }else if(req.url=='/student'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p style="color:blue">This is student Page.</p></body></html>');
+        res.end();
+    }else{
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p style="color:blue">Other pages</p></body></html>');
+        res.end();
+    }
+    
 });
 server.listen(5000);
 console.log('server is running!');
