fbc9947a722db440e2a809933c8a00bc5d3adfde
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-05-27 11:13:42 +0700

    add Express

---
+++ expressDemo/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var app = express();
+
+// define routes here..
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+app.get('/test', function (req, res) {
+    res.sendFile(__dirname + '/test.html');
+});
+
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
--- webApp/app.js
+++ webApp/app.js
@@ -6,7 +6,7 @@ var server = http.createServer((req,res)=>{
         res.end();
     }else if(req.url=='/student'){
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p style="color:blue">This is student Page.</p></body></html>');
+        res.write('<html><body><input type="text" size="20"/><input type="button" value="Login"></body></html>');
         res.end();
     }else{
         res.writeHead(200, { 'Content-Type': 'text/html' });
