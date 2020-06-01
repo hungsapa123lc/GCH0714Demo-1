b0952865307f5cfcf286db2370892138c271f165
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-06-01 08:18:22 +0700

    aaa

--- expressDemo/app.js
+++ expressDemo/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 var app = express();
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // define routes here..
 app.get('/', function (req, res) {
     res.send('<html><body><h1>Hello World</h1></body></html>');
 });
-app.get('/testHtml', function (req, res) {
-    res.sendFile(__dirname + '/test.html');
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '/login.html');
 });
+
+app.post('/doLogin',(req,res)=>{
+    let name = req.body.txtName;
+    let password = req.body.txtPassword;
+    if(password =='123@'){
+        res.end('Login Ok');
+    }else{
+        res.end('Login failed!');
+    }
+})
 
 
 var server = app.listen(5000, function () {
