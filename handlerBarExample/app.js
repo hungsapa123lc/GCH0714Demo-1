ce2b7b47d8e966fcc6498f40fcb95de720dd3007
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-06-03 10:02:20 +0700

    add login

--- handlerBarExample/app.js
+++ handlerBarExample/app.js
@@ -7,6 +7,9 @@ app.engine('hbs',engines.handlebars);
 app.set('views','./views');
 app.set('view engine','hbs');
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function (req, res) {
     res.render('index',{name:"Mr.Bean",email:"Bean@gmail.com"});
 
@@ -14,7 +17,11 @@ app.get('/', function (req, res) {
 app.get('/login',(req,res)=>{
     res.render('login');
 })
-
+app.post('/doLogin',(req,res)=>{
+    let inputName = req.body.txtName;
+    let inputEmail = req.body.txtEmail;
+    res.render('index',{name:inputName,email:inputEmail});
+})
 var server = app.listen(5000, function () {
     console.log('Node server is running..');
 });
